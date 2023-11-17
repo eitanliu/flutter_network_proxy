8b4bf645f00c92b7a9b83d82ac08e8b54092c324
Eitan Liu <eitanliu@gmail.com>
2023-11-17 17:32:47 +0800

    moved Android and iOS impl

---
+++ network_proxy_android/lib/network_proxy_android.dart
@@ -0,0 +1,4 @@
+import 'package:network_proxy_interface/network_proxy_method_channel.dart';
+
+/// A web implementation of the NetworkProxyPlatform of the NetworkProxy plugin.
+class NetworkProxyAndroid extends MethodChannelNetworkProxy {}
